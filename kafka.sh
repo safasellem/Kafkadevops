3b98e435d157990ab389d26e0758b51842bdce7a
safasellem <safasellem@gmail.com>
2024-01-27 07:49:27 -0500

    premier commit

--- kafka.sh
+++ kafka.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sudo apt-get update
 if ! dpkg -l | grep -q "python3"; then
   sudo apt-get install -y python3
@@ -9,7 +10,6 @@ if ! dpkg -l | grep -q "python3-pip"; then
 else
   echo "Package 'python3-pip' is already installed"
 fi
-
 if ! dpkg -l | grep -q "docker"; then
   sudo apt-get install -y docker
 else
@@ -21,10 +21,10 @@ else
   echo "Package 'docker-compose' is already installed"
 fi
 cd /home/osboxes/kafka-docker
-sudo python3 -m venv venv
+sudo python3  -m venv venv
 sudo source venv/bin/activate
 if ! pip3 show kafka-python > /dev/null; then
-  sudo pip install kafka-python
+  sudo pip3 install kafka-python
 else
   echo "Package 'kafka-python' is already installed"
 fi
@@ -32,13 +32,9 @@ fi
 if [ -d "kafka-docker" ]; then
   sudo rm -rf kafka-docker
 fi
-
 sudo git clone https://github.com/wurstmeister/kafka-docker.git
-
 cd kafka-docker/
-
 filename="docker-compose-expose.yml"
-
 cat << EOF > $filename
 version: '2'
 services:
@@ -47,7 +43,9 @@ services:
     ports:
      - "2181:2181"
   kafka:
-    build: .
+    image: wurstmeister/kafka
+    depends_on:
+    - zookeeper
     ports:
      - "9092:9092"
     expose:
@@ -60,10 +58,9 @@ services:
       KAFKA_ZOOKEEPER_CONNECT: zookeeper:2181
       KAFKA_CREATE_TOPICS: "topic_test:1:1"
     volumes:
-     - /var/run/docker.sock:/var/run/docker.sock
+     - /var/run/docker.sock:/var/run/docker.sock   k
 EOF
-
 echo "YAML file $filename has been created."
-
 sudo docker-compose -f docker-compose-expose.yml up
+
 
--- producer.py
+++ producer.py
@@ -1,69 +1,11 @@
-sudo apt-get update
-if ! dpkg -l | grep -q "python3"; then
-  sudo apt-get install -y python3
-else
-  echo "Package 'python3' is already installed"
-fi
-if ! dpkg -l | grep -q "python3-pip"; then
-  sudo apt-get install -y python3-pip
-else
-  echo "Package 'python3-pip' is already installed"
-fi
-
-if ! dpkg -l | grep -q "docker"; then
-  sudo apt-get install -y docker
-else
-  echo "Package 'docker' is already installed"
-fi
-if ! dpkg -l | grep -q "docker-compose"; then
-  sudo apt-get install -y docker-compose
-else
-  echo "Package 'docker-compose' is already installed"
-fi
-cd /home/osboxes/kafka-docker
-sudo python3 -m venv venv
-sudo source venv/bin/activate
-if ! pip3 show kafka-python > /dev/null; then
-  sudo pip install kafka-python
-else
-  echo "Package 'kafka-python' is already installed"
-fi
-
-if [ -d "kafka-docker" ]; then
-  sudo rm -rf kafka-docker
-fi
-
-sudo git clone https://github.com/wurstmeister/kafka-docker.git
-
-cd kafka-docker/
-
-filename="docker-compose-expose.yml"
-
-cat << EOF > $filename
-version: '2'
-services:
-  zookeeper:
-    image: wurstmeister/zookeeper:3.4.6
-    ports:
-     - "2181:2181"
-  kafka:
-    build: .
-    ports:
-     - "9092:9092"
-    expose:
-     - "9093"
-    environment:
-      KAFKA_ADVERTISED_LISTENERS: INSIDE://kafka:9093,OUTSIDE://localhost:9092
-      KAFKA_LISTENER_SECURITY_PROTOCOL_MAP: INSIDE:PLAINTEXT,OUTSIDE:PLAINTEXT
-      KAFKA_LISTENERS: INSIDE://0.0.0.0:9093,OUTSIDE://0.0.0.0:9092
-      KAFKA_INTER_BROKER_LISTENER_NAME: INSIDE
-      KAFKA_ZOOKEEPER_CONNECT: zookeeper:2181
-      KAFKA_CREATE_TOPICS: "topic_test:1:1"
-    volumes:
-     - /var/run/docker.sock:/var/run/docker.sock
-EOF
-
-echo "YAML file $filename has been created."
-
-sudo docker-compose -f docker-compose-expose.yml up
-
+from time import sleep
+from json import dumps
+from kafka import KafkaProducer
+producer = KafkaProducer(
+    bootstrap_servers=['localhost:9092'],
+    value_serializer=lambda x: dumps(x).encode('utf-8'))
+for j in range(9999):
+    print("Iteration", j)
+    data = {'counter': j}
+    producer.send('topic_test', value=data)
+    sleep(0.5)
